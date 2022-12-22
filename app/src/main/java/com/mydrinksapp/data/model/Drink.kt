887e59b75a87d0e3af33ab8faab833f8b3c83f77
Gustavo Maldonado <gamcba7982@gmail.com>
2022-12-22 19:31:13 -0300

    room entity created

--- app/src/main/java/com/mydrinksapp/data/model/Drink.kt
+++ app/src/main/java/com/mydrinksapp/data/model/Drink.kt
@@ -1,11 +1,15 @@
 package com.mydrinksapp.data.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Drink(
+    @SerializedName("idDrink") val tragoId: String = "",
     @SerializedName("strDrinkThumb") val imagen: String = "",
     @SerializedName("strDrink") val nombre: String = "",
     @SerializedName("strInstructions") val descripcion: String = "",
@@ -14,4 +18,13 @@ data class Drink(
 
 data class DrinkList(
     @SerializedName("drinks") val drinkList: List<Drink>
+)
+
+@Entity
+data class DrinkEntity(
+    @PrimaryKey val tragoId: String,
+    @ColumnInfo(name = "trago_imagen") val imagen: String = "",
+    @ColumnInfo(name = "trago_nombre") val nombre: String = "",
+    @ColumnInfo(name = "trago_descripcion") val descripcion: String = "",
+    @ColumnInfo(name = "trago_has_alcohol") val hasAlcohol: String = "Non_Alcoholic"
 )
