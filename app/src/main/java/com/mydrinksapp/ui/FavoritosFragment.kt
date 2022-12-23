fb40d198de0498425fab2e6976b531310f28da56
Gustavo Maldonado <gamcba7982@gmail.com>
2022-12-22 21:15:17 -0300

    show favourite fragment

--- app/src/main/java/com/mydrinksapp/ui/FavoritosFragment.kt
+++ app/src/main/java/com/mydrinksapp/ui/FavoritosFragment.kt
@@ -1,23 +1,25 @@
 package com.mydrinksapp.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mydrinksapp.AppDatabase
 import com.mydrinksapp.R
 import com.mydrinksapp.data.DataSource
+import com.mydrinksapp.data.model.Drink
 import com.mydrinksapp.databinding.FragmentFavoritosBinding
 import com.mydrinksapp.domain.RepoImpl
 import com.mydrinksapp.ui.viewmodel.MainViewModel
 import com.mydrinksapp.ui.viewmodel.VMFactory
 import com.mydrinksapp.vo.Resource
 
-class FavoritosFragment : Fragment() {
+class FavoritosFragment : Fragment(), MainAdapter.OnTragoClickListener {
 
     private lateinit var binding: FragmentFavoritosBinding
     private val viewModel by activityViewModels<MainViewModel> {
@@ -45,14 +47,38 @@ class FavoritosFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentFavoritosBinding.bind(view)
 
+        setupRecyclerView()
+        setupObserver()
+    }
+
+    private fun setupObserver() {
         viewModel.getTragosFavoritos().observe(viewLifecycleOwner, Observer { result ->
             when (result) {
                 is Resource.Loading -> {}
                 is Resource.Success -> {
-                    Log.d("LISTA DE FAVORITOS: ", "${result.data}")
+                    val lista: List<Drink> = result.data.map {
+                        Drink(it.tragoId, it.imagen, it.nombre, it.descripcion, it.hasAlcohol)
+                    }
+                    binding.rvTragosFavoritos.adapter = MainAdapter(requireContext(), lista, this)
+
+                    //Log.d("LISTA DE FAVORITOS: ", "${result.data}")
                 }
                 is Resource.Failure -> {}
             }
         })
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvTragosFavoritos.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTragosFavoritos.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.HORIZONTAL
+            )
+        )
+    }
+
+    override fun onTragoClick(drink: Drink) {
+
     }
 }
