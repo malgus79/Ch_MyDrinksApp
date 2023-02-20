2c3c4d83f18cadca2f51fe43949549e274cbb99d
Gustavo Maldonado <gamcba7982@gmail.com>
2023-02-20 18:45:12 -0300

    Setup handleOnBackPressed in home

--- app/src/main/java/com/mydrinksapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mydrinksapp/ui/home/HomeFragment.kt
@@ -4,18 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import com.mydrinksapp.R
+import com.mydrinksapp.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        onBackPressedCallback()
+
+        return binding.root
+    }
+
+    private fun onBackPressedCallback() {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    requireActivity().finish()
+                }
+            }
+        )
     }
 }
